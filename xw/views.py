8dffb58b2afb1bed9a8791219169e819f270c6fc
xw <xw@xw.com>
2013-10-20 15:06:03 -0700

    Incremental commit. - UI looks roughly reasonable
    Still to come:
    - Numbering should be more accurate
    - Some place for the clues to go
    - Selecting clues should select numbers too, and so on
    - Be able to type answers in horizontally and vertically

--- xw/views.py
+++ xw/views.py
@@ -1,13 +1,13 @@
-from pyramid.response import Response
+import itertools
 from pyramid.view import view_config
-
-from sqlalchemy.exc import DBAPIError
-
-from .models import (
-    DBSession,
-    MyModel,
-    )
 
 @view_config(route_name='index', renderer='templates/index.jinja2')
 def index(request):
-    return {'project': 'xw'}
+    s = '----.----.---------.----.---------.----.-----------------....-----...------...-----------------.----..--------.---.--------..----.-----------------...------...-----....-----------------.----.---------.----.---------.----.----'
+    lines = []
+    LINE_SIZE = 15
+    for i in itertools.count(0, LINE_SIZE):
+        if i >= len(s):
+            break
+        lines.append(s[i:i+LINE_SIZE])
+    return {'project': 'xw', 'lines': lines}
